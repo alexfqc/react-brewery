8249d6c3ba15e6feb79a66caeaddef5a0e5611f5
Alessander Franca <alessander70@gmail.com>
2018-02-06 22:22:44 -0200

    ContainerList component created, tests updated, enzyme package added

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ContainerList from './container/list/ContainerList';
+
+const App = () => (
+  <div>
+    <h2>BREVERY ONLINE</h2>
+    <ContainerList />
+  </div>
+);
+
+export default App;
